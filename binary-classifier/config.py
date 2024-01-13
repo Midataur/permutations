f74d04b45dca75878ec82fb8bbbc94de58ef982e
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-13 11:20:52 +1100

    Added more data

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -1,6 +1,6 @@
 MAX_LENGTH = 10
 GROUP_SIZE = 5
-DATASET_SIZE = 100_000
+DATASET_SIZE = 300_000
 IDENTITY_PROPORTION = 0.5 #controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/forcegrok/"
