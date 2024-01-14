72706430650d22d73a91f06f6a3c25cf5ee3f505
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-14 14:28:59 +1100

    increased dataset size

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -4,5 +4,5 @@ DATASET_SIZE = 1_600_000
 IDENTITY_PROPORTION = 0.5 #controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/forcegrok/"
-MODELNAME = "forcegrok3.0"
+MODELNAME = "forcegrok4.0"
 TRAIN_FILES = 7
