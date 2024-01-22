925b142e6fba2118555e88d332f67da1da557fee
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-23 10:51:00 +1100

    updated model

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -3,4 +3,4 @@ GROUP_SIZE = 10
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/biggrok/"
-MODELNAME = "biggrok3.0"
+MODELNAME = "biggrok4.0"
