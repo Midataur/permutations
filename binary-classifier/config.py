ad0e91f425bb147dbd4852ae2dd38746344b9b5a
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-27 14:09:05 +1100

    config change

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -3,4 +3,4 @@ GROUP_SIZE = 5
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/forcegrok/"
-MODELNAME = "forcegrok8.0"
+MODELNAME = "forcegrok9.0"
