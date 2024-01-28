970aca7f4ad17509eabb37ec5a1da514dc9f2099
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-29 09:32:02 +1100

    increase embedding dimension

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -4,16 +4,16 @@ GROUP_SIZE = 4
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/verysmall/"
-MODELNAME = "smallgrok1.0"
+MODELNAME = "smallgrok2.0"
 # can be full or an integer
 # i recommend 64
 BATCHSIZE = "full"
 
 # TRANSFORMER HYPERPARAMETERS
-n_embed = 50
+n_embed = 384
 vocab_size = GROUP_SIZE
 block_size = MAX_LENGTH
-n_head = 5
+n_head = 6
 n_blocks = 1
 dropout = 0
 
