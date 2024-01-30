a2f56f4cf75ae57110764d36d2ab2f3cb8272872
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-01-31 08:41:00 +1100

    whoops

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -3,7 +3,7 @@ MAX_LENGTH = 6
 GROUP_SIZE = 4
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
-DATA = "/data/verysmall/"
+DATA = "/data/smallgrok/"
 MODELNAME = "smallgrok5.0"
 # can be full or an integer
 # i recommend 64
