923ab96fc8a0107033e39566a8eba8c3c3b613a4
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-02-03 15:15:34 +1100

    medtrans-6.0

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -4,7 +4,7 @@ GROUP_SIZE = 5
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/medtrans/"
-MODELNAME = "medtrans-5.0"
+MODELNAME = "medtrans-6.0"
 # can be full or an integer
 # i recommend 64
 BATCHSIZE = 64
@@ -14,7 +14,7 @@ n_embed = 384
 vocab_size = GROUP_SIZE**2
 block_size = MAX_LENGTH
 n_head = 6
-n_blocks = 2
+n_blocks = 1
 dropout = 0.1
 
 # TRAINING HYPERPARAMETERS
