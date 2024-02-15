dce7dd8574c621d282369192919a82d89f5cc1b9
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-02-15 11:55:09 +1100

    forcegrok15.0

--- binary-classifier/config.py
+++ binary-classifier/config.py
@@ -4,7 +4,7 @@ GROUP_SIZE = 5
 IDENTITY_PROPORTION = 0.5 # controls what proportion of the training data is artifically added identities
 PATH = "."
 DATA = "/data/forcegrok/"
-MODELNAME = "forcegrok14.0"
+MODELNAME = "forcegrok15.0"
 # can be "full" or an integer
 # i recommend 64
 BATCHSIZE = 64
@@ -26,7 +26,7 @@ learning_rate = 3*(10**-5)
 num_epochs = 10**8
 
 # good starting value: 0.01
-weight_decay = 1
+weight_decay = 0.1
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
