aa584a3f76c1ee8f39f4d34c77cfc394266796cb
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-06-07 14:41:13 +1000

    bin-medium-7

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -17,7 +17,7 @@ INPUT_LENGTH = (int(log2(MAX_GROUP_SIZE)) + 1) * 2 * MAX_TRANS_NUMBER
 CONTEXT_LENGTH = INPUT_LENGTH + 1 + MAX_GROUP_SIZE + 1
 PATH = "."
 DATA = "/data/bin_medium/"
-MODELNAME = "bin-medium-6.0"
+MODELNAME = "bin-medium-7.0"
 # can be "full" or an integer
 # i recommend 64
 BATCHSIZE = 64
@@ -37,7 +37,7 @@ END_PREDICTION_TOKEN = num_normal + 2
 
 # TRANSFORMER HYPERPARAMETERS
 vocab_size = num_normal + num_special
-n_embed = 24
+n_embed = 48
 block_size = CONTEXT_LENGTH
 n_head = 6
 n_blocks = 16
