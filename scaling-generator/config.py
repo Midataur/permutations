60a2be0700bf1f34b0cc21e375a00da81f5ed84f
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-07-05 10:41:25 +1000

    elementary-1

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -7,12 +7,16 @@ MAX_GROUP_SIZE = 16
 ACTUAL_GROUP_SIZE = 10
 WINDOW = True
 
+PATH = "."
+DATA = "/data/elementary/"
+MODELNAME = "elementary-1.0"
+
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
-LEGACY_OVERRIDE = True
+LEGACY_OVERRIDE = False
 # used for backwards compatability with an older version of datagen
 # adds one to the number of digits used for a binary number
-DIGIT_OVERRIDE = True
+DIGIT_OVERRIDE = False
 
 # the maximum number of transpositions in the input sequence
 MAX_TRANS_NUMBER = 10
@@ -21,7 +25,7 @@ MAX_TRANS_NUMBER = 10
 # can be general (one token per transposition, general transpositions allowed)
 # can be hybrid (two tokens per transposition, general transpositions allowed)
 # or binary (each tranposition is written in binary)
-INPUT_TYPE = "binary"
+INPUT_TYPE = "elementary"
 # maximum length of input sequence (in tokens)
 # don't touch this
 
@@ -44,13 +48,6 @@ CONTEXT_LENGTH = INPUT_LENGTH + 1 + MAX_GROUP_SIZE
 
 if LEGACY_OVERRIDE:
     CONTEXT_LENGTH += 1 # allows for deprecated end_sequence_token
-
-PATH = "."
-DATA = "/data/window_test/"
-MODELNAME = "window-7.0"
-# can be "full" or an integer
-# i recommend 64
-BATCHSIZE = 64
 
 # TOKENS
 # do not change unless you're max
@@ -79,13 +76,16 @@ vocab_size = num_normal + num_special
 n_embed = 102
 block_size = CONTEXT_LENGTH
 n_head = 6
-n_blocks = 8
+n_blocks = 4
 dropout = 0
 
 # TRAINING HYPERPARAMETERS
 # good starting value: 3*10^-5
 learning_rate = 3*(10**-4)
 num_epochs = 10**8
+# can be "full" or an integer
+# i recommend 64
+BATCHSIZE = 64
 
 # good starting value: 0.01
 weight_decay = 0.01
