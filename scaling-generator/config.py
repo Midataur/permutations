1dfd31d7036a81a9fe7dcabef1824e0026052131
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-07-13 15:04:55 +1000

    hybrid-big-4.0

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -10,7 +10,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/hybrid_big/"
-MODELNAME = "hybrid-big-3.0"
+MODELNAME = "hybrid-big-4.0"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -71,10 +71,10 @@ END_PREDICTION_TOKEN = num_normal + 2
 # TRANSFORMER HYPERPARAMETERS
 # you can change these if you want
 vocab_size = num_normal + num_special
-n_embed = 402
+n_embed = 606
 block_size = CONTEXT_LENGTH
 n_head = 6
-n_blocks = 8
+n_blocks = 4
 dropout = 0
 
 # TRAINING HYPERPARAMETERS
@@ -86,7 +86,7 @@ num_epochs = 10**8
 BATCHSIZE = 64
 
 # good starting value: 0.01
-weight_decay = 0.005
+weight_decay = 0.01
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
