1571c44689495cd7c9242dac06bb2811a6e780a3
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-07-24 12:05:04 +1000

    hybrid-big-13

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -11,7 +11,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/hybrid_big/"
-MODELNAME = "hybrid-big-12"
+MODELNAME = "hybrid-big-13"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -75,7 +75,7 @@ vocab_size = num_normal + num_special
 n_embed = 402
 block_size = CONTEXT_LENGTH
 n_head = 6
-n_blocks = 8
+n_blocks = 4
 dropout = 0
 
 # TRAINING HYPERPARAMETERS
@@ -87,7 +87,7 @@ num_epochs = 10**8
 BATCHSIZE = 256
 
 # good starting value: 0.01
-weight_decay = 0.1
+weight_decay = 0.001
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
