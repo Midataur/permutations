5bc1f2f5552c4fd20785fdf86f05c65fc963f0c2
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-09-27 08:49:17 +1000

    hybrid-big-36

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -11,7 +11,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/hybrid_big/"
-MODELNAME = "hybrid-big-35"
+MODELNAME = "hybrid-big-36"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -72,7 +72,7 @@ END_PREDICTION_TOKEN = num_normal + 2
 # TRANSFORMER HYPERPARAMETERS
 # you can change these if you want
 vocab_size = num_normal + num_special
-n_embed = 600
+n_embed = 800
 block_size = CONTEXT_LENGTH
 n_head = 4
 n_blocks = 8
