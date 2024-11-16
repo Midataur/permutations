3e29bce6983f2fe4020ec0df679b1ee9cc5415c1
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-11-17 10:05:09 +1100

    hybrid-big-72

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -11,7 +11,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/hybrid_big/"
-MODELNAME = "hybrid-big-71"
+MODELNAME = "hybrid-big-72"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -75,12 +75,12 @@ vocab_size = num_normal + num_special
 n_embed = 402
 block_size = CONTEXT_LENGTH
 n_head = 6
-n_blocks = 8
+n_blocks = 4
 dropout = 0
 
 # TRAINING HYPERPARAMETERS
 # good starting value: 3*10^-5
-learning_rate = 3*(10**-6)
+learning_rate = 3*(10**-4)
 num_epochs = 10**8
 # can be "full" or an integer
 # good starting value: 64
