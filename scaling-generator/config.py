396fd842ec5c6018532a51932e271074fd71dabf
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-11-20 08:52:36 +1100

    hybrid-big-82

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -11,7 +11,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/hybrid_big/"
-MODELNAME = "hybrid-big-81"
+MODELNAME = "hybrid-big-82"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -87,7 +87,7 @@ num_epochs = 10**8
 BATCHSIZE = 1024
 
 # good starting value: 0.01
-weight_decay = 0.005
+weight_decay = 0.01
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
