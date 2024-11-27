a5dd4a5d898b46c098f42f03cf813803ed47c2ed
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-11-27 18:56:28 +1100

    probe test 1

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -11,7 +11,7 @@ RELABEL = True
 
 PATH = "."
 DATA = "/data/torn_big/"
-MODELNAME = "torn-big-15"
+MODELNAME = "wackotestaroony"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
--- scaling-generator/probe_main.py
+++ scaling-generator/probe_main.py
@@ -1,6 +1,8 @@
 print("Loading libraries...")
 
 from training import train
+from probed_transfomer import ProbedTransformer
+from dataloading import ProbeDataset
 from config import *
 
 # check if the last element is unchanged
@@ -8,4 +10,10 @@ def question(permutation):
     return permutation[-1] == MAX_GROUP_SIZE-1
 
 if __name__ == "__main__":
-    train()
+    train(
+        model_class=ProbedTransformer,
+        dataset_class=ProbeDataset,
+        question=question,
+        stop_block=0,
+        suffix="-probed"
+    )
