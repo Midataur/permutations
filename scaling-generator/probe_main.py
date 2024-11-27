316a89c7ceff034055a906215706c8a1ad9fd77d
Max Petschack <18609293+Midataur@users.noreply.github.com>
2024-11-27 19:04:17 +1100

    quick fix

--- scaling-generator/probe_main.py
+++ scaling-generator/probe_main.py
@@ -7,7 +7,7 @@ from config import *
 
 # check if the last element is unchanged
 def question(permutation):
-    return permutation[-1] == MAX_GROUP_SIZE-1
+    return int(permutation[-1] == MAX_GROUP_SIZE-1)
 
 if __name__ == "__main__":
     train(
