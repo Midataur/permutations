5738767836913991393d7b005e55395d5032b5fc
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-01-01 12:25:26 +1100

    elem-big-2

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -87,7 +87,7 @@ num_epochs = 10**8
 BATCHSIZE = 1024
 
 # good starting value: 0.01
-weight_decay = 0.01
+weight_decay = 0.02
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
