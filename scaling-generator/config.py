8c261f3805455f3459cb4a3f13414a9257d94579
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-02-08 18:54:33 +1100

    torn-bigger-12

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -20,7 +20,7 @@ PATH = "."
 # MODELNAME = "elem-partitioned-long-8"
 
 DATA = "/data/torn_bigger/"
-MODELNAME = "torn-bigger-11"
+MODELNAME = "torn-bigger-12"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -105,7 +105,7 @@ num_epochs = 10**8
 BATCHSIZE = 1024
 
 # good starting value: 0.01
-weight_decay = 0.01
+weight_decay = 0.001
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
