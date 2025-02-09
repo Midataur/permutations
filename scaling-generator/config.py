4bc63d734b70b4387a0fc1d7428031c92f93c679
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-02-09 12:13:33 +1100

    elem-long-partitioned-10

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -5,7 +5,7 @@ import torch
 set_seed(42)
 
 MAX_GROUP_SIZE = 16
-ACTUAL_GROUP_SIZE = 10
+ACTUAL_GROUP_SIZE = 9
 
 # MAX_GROUP_SIZE = 25
 # ACTUAL_GROUP_SIZE = 10
@@ -16,8 +16,8 @@ PARTITIONED_WINDOWS = True
 RELABEL = False
 
 PATH = "."
-DATA = "/data/torn/"
-MODELNAME = "torn-masked-11"
+DATA = "/data/elem_partitioned_long/"
+MODELNAME = "elem-partitioned-long-10"
 
 # DATA = "/data/torn_bigger/"
 # MODELNAME = "torn-bigger-12"
@@ -35,14 +35,13 @@ DIGIT_OVERRIDE = False
 MASKED_MODEL = True
 
 # the maximum number of transpositions in the input sequence
-MAX_TRANS_NUMBER = 10
-# MAX_TRANS_NUMBER = 25
+MAX_TRANS_NUMBER = 120
 
 # can be elementary (one token per transposition, only adjacent transpositions allowed)
 # can be general (one token per transposition, general transpositions allowed)
 # can be hybrid (two tokens per transposition, general transpositions allowed)
 # or binary (each tranposition is written in binary)
-INPUT_TYPE = "general"
+INPUT_TYPE = "elementary"
 
 # maximum length of input sequence (in tokens)
 # don't touch this
@@ -98,7 +97,7 @@ dropout = 0
 
 # TRAINING HYPERPARAMETERS
 # good starting value: 3*10^-5
-learning_rate = 3*(10**-5)
+learning_rate = 3*(10**-4)
 num_epochs = 10**8
 # can be "full" or an integer
 # good starting value: 64
