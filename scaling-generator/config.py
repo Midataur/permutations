41bd592cf2cf0cbb12654377bfc658297151d3fd
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-02-10 17:53:00 +1100

    elem-long-partitioned-14

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -17,7 +17,7 @@ RELABEL = False
 
 PATH = "."
 DATA = "/data/elem_partitioned_long/"
-MODELNAME = "elem-partitioned-long-13"
+MODELNAME = "elem-partitioned-long-14"
 
 # DATA = "/data/torn_bigger/"
 # MODELNAME = "torn-bigger-12"
@@ -104,7 +104,7 @@ num_epochs = 10**8
 BATCHSIZE = 1024
 
 # good starting value: 0.01
-weight_decay = 0.01
+weight_decay = 0.05
 
 lr_factor = 0.1  # Factor by which the learning rate will be reduced
 lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
