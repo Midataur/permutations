becbfb985caa46802635b42d49cd17161bdf1597
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-07-10 20:13:17 +1000

    changed config

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -14,8 +14,8 @@ RELABEL = False
 
 PATH = "."
 
-DATA = "/data/reverse/"
-MODELNAME = "reverse-4"
+DATA = "/data/elem-partitioned-long-boosted/"
+MODELNAME = "elem-partitioned-long-boosted-1"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -31,7 +31,7 @@ MASKED_MODEL = True
 
 # are we doing the reverse problem?
 # ie. predict the reduced word from the permutation
-REVERSE_PROBLEM = True
+REVERSE_PROBLEM = False
 
 # used for backwards compatability with older models
 # we used to use a slightly weird non-standard architecture
@@ -55,63 +55,3 @@ if INPUT_TYPE == "binary":
         DIGITS_USED += 1
 
     INPUT_LENGTH = DIGITS_USED * 2 * MAX_TRANS_NUMBER
-elif INPUT_TYPE == "hybrid":
-    INPUT_LENGTH = MAX_TRANS_NUMBER * 2
-else:
-    INPUT_LENGTH = MAX_TRANS_NUMBER
-
-CONTEXT_LENGTH = INPUT_LENGTH + MAX_GROUP_SIZE
-
-if LEGACY_OVERRIDE:
-    CONTEXT_LENGTH += 1 # allows for deprecated end_sequence_token
-
-if not MASKED_MODEL:
-    CONTEXT_LENGTH += 1 # allows for deprecated final permutation token
-
-# TOKENS
-# do not change unless you're max
-if INPUT_TYPE in ["elementary", "hybrid"]:
-    num_trans = MAX_GROUP_SIZE
-elif INPUT_TYPE == "general":
-    num_trans = MAX_GROUP_SIZE**2
-elif INPUT_TYPE == "binary":
-    num_trans = 2
-
-num_normal = num_trans + MAX_GROUP_SIZE
-
-num_special = 2
-if LEGACY_OVERRIDE:
-    num_special = 3
-
-NULL_TOKEN = num_normal
-START_PREDICTION_TOKEN = num_normal + 1
-
-# this is deprecated and should no longer be used
-END_PREDICTION_TOKEN = num_normal + 2
-
-# TRANSFORMER HYPERPARAMETERS
-# you can change these if you want
-vocab_size = num_normal + num_special
-n_embed = 402
-block_size = CONTEXT_LENGTH
-n_head = 6
-n_blocks = 10
-dropout = 0
-
-# TRAINING HYPERPARAMETERS
-# good starting value: 3*10^-5
-learning_rate = 3*(10**-4)
-num_epochs = 10**8
-# can be "full" or an integer
-# good starting value: 64
-BATCHSIZE = 1024
-
-# good starting value: 0.01
-weight_decay = 0.01
-
-lr_factor = 0.1  # Factor by which the learning rate will be reduced
-lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
-threshold = 0.01  # Threshold for measuring the new optimum
-
-# for dataloading
-N_WORKERS = 0
