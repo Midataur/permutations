77dc5527d77fbab9e31f9a1001bcf33f87801fb0
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-07-10 20:25:27 +1000

    whoops

--- scaling-generator/config.py
+++ scaling-generator/config.py
@@ -2,7 +2,7 @@ from math import log2, floor
 from accelerate.utils import set_seed
 import torch
 
-set_seed(42)
+set_seed(97)
 
 MAX_GROUP_SIZE = 16
 ACTUAL_GROUP_SIZE = 10
@@ -15,7 +15,7 @@ RELABEL = False
 PATH = "."
 
 DATA = "/data/elem-partitioned-long-boosted/"
-MODELNAME = "elem-partitioned-long-boosted-1"
+MODELNAME = "elem-partitioned-long-boosted-4"
 
 # used to enable legacy features that have been deprecated
 # this is for backwards compatability reasons
@@ -55,3 +55,63 @@ if INPUT_TYPE == "binary":
         DIGITS_USED += 1
 
     INPUT_LENGTH = DIGITS_USED * 2 * MAX_TRANS_NUMBER
+elif INPUT_TYPE == "hybrid":
+    INPUT_LENGTH = MAX_TRANS_NUMBER * 2
+else:
+    INPUT_LENGTH = MAX_TRANS_NUMBER
+
+CONTEXT_LENGTH = INPUT_LENGTH + MAX_GROUP_SIZE
+
+if LEGACY_OVERRIDE:
+    CONTEXT_LENGTH += 1 # allows for deprecated end_sequence_token
+
+if not MASKED_MODEL:
+    CONTEXT_LENGTH += 1 # allows for deprecated final permutation token
+
+# TOKENS
+# do not change unless you're max
+if INPUT_TYPE in ["elementary", "hybrid"]:
+    num_trans = MAX_GROUP_SIZE
+elif INPUT_TYPE == "general":
+    num_trans = MAX_GROUP_SIZE**2
+elif INPUT_TYPE == "binary":
+    num_trans = 2
+
+num_normal = num_trans + MAX_GROUP_SIZE
+
+num_special = 2
+if LEGACY_OVERRIDE:
+    num_special = 3
+
+NULL_TOKEN = num_normal
+START_PREDICTION_TOKEN = num_normal + 1
+
+# this is deprecated and should no longer be used
+END_PREDICTION_TOKEN = num_normal + 2
+
+# TRANSFORMER HYPERPARAMETERS
+# you can change these if you want
+vocab_size = num_normal + num_special
+n_embed = 402
+block_size = CONTEXT_LENGTH
+n_head = 6
+n_blocks = 5
+dropout = 0
+
+# TRAINING HYPERPARAMETERS
+# good starting value: 3*10^-5
+learning_rate = 3*(10**-4)
+num_epochs = 10**8
+# can be "full" or an integer
+# good starting value: 64
+BATCHSIZE = 1024
+
+# good starting value: 0.01
+weight_decay = 0.05
+
+lr_factor = 0.1  # Factor by which the learning rate will be reduced
+lr_patience = 10  # Number of epochs with no improvement after which learning rate will be reduced
+threshold = 0.01  # Threshold for measuring the new optimum
+
+# for dataloading
+N_WORKERS = 0
